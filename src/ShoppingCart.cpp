df6af55d99f7063b40b66e665b3fafd2da729b03
Pavan Kumar Byri <pbyri@qti.qualcomm.com>
2018-06-11 22:35:08 -0700

    fixed removeFromCart method

--- src/ShoppingCart.cpp
+++ src/ShoppingCart.cpp
@@ -46,10 +46,12 @@ void ShoppingCart::addToCart(std::unique_ptr<PurchaseOrder> order)
 
 void ShoppingCart::removeFromCart(uint16_t order_id)
 {
-  std::remove_if(m_orders.begin(),
-                 m_orders.end(),
-		 [order_id](auto &order) { return order->getOrderNumber() == order_id; }
-		 );
+  m_orders.erase(std::remove_if(m_orders.begin(),
+                               m_orders.end(),
+                               [&order_id](auto &order) { return order->getOrderNumber() == order_id; }
+                               ),
+                m_orders.end()
+                );
 }
 
 uint16_t ShoppingCart::getNumberOfPurchaseOrders() const
@@ -70,6 +72,8 @@ TEST_CASE("verify PurchaseOrder object creation","[PurchaseOrder]")
                                               3);
   REQUIRE(order);
   REQUIRE(order.get());
+  REQUIRE(order->getOrderNumber() == 12254);
+  REQUIRE(order->getCost() == 3 * 3.33);
 }
 TEST_CASE("verify ShoppingCart object creation","[ShoppingCart]")
 {
@@ -77,6 +81,7 @@ TEST_CASE("verify ShoppingCart object creation","[ShoppingCart]")
 
   REQUIRE(cart);
   REQUIRE(cart.get());
+  REQUIRE(cart->getNumberOfPurchaseOrders() == 0);
 }
 SCENARIO("verify adding and removing purchase order to/from the cart",\
          "[ShoppingCart]")
@@ -85,6 +90,7 @@ SCENARIO("verify adding and removing purchase order to/from the cart",\
   {
     auto cart = std::make_unique<ShoppingCart>();
     REQUIRE(cart.get());
+    REQUIRE(cart->getNumberOfPurchaseOrders() == 0);
     WHEN("A purchase order is added to the cart")
     {
       auto order = std::make_unique<PurchaseOrder>(12254,
@@ -107,8 +113,10 @@ SCENARIO("verify adding and removing purchase order to/from the cart",\
                                                                            3.33),
                                                   3);
       REQUIRE(order.get());
+      REQUIRE(cart->getNumberOfPurchaseOrders() == 0);
+      auto order_num = order->getOrderNumber();
       cart->addToCart(std::move(order));
-      cart->removeFromCart(12254);
+      cart->removeFromCart(order_num);
       THEN("The size of the cart goes down by 1")
       {
         REQUIRE(cart->getNumberOfPurchaseOrders() == 0);
