ff2e11a9697199122b1123ae7509b7361b3e0b35
pavansemail@gmail.com <pavansemail@gmail.com>
2018-06-13 08:00:31 -0700

    added comments to SuperMarket.cpp file

--- src/SuperMarket.cpp
+++ src/SuperMarket.cpp
@@ -1,3 +1,9 @@
+/** @file SuperMarket.cpp
+*   @brief This is the entry point file for this project
+*   @author Pavan Kumar Byri
+*   @date 06-12-2018
+*/
+
 #include <iostream>
 #include <Store.h>
 #include <memory>
@@ -9,10 +15,14 @@
 
 using namespace SuperStore;
 #ifndef TEST_SUPER_MARKET
+// Just create an instance of Store and invoke launch() method on it.
+// Let it handle the rest. When the control returns back, print an exiting
+// message and exit.
 int main()
 {
   auto store = std::make_unique<Store>("Joe\'s");
   store->launch();
+  std::cout<<"Exiting application..";
   return 0;  
 }
 
